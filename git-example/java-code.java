8f673f433ef9f8cc449a5257805fe86efe56fb3b
Parthasarathy-G <parthasarathy0580@gmail.com>
2025-01-17 16:25:09 +0530

    Java File changed

--- git-example/java-code.java
+++ git-example/java-code.java
@@ -3,5 +3,7 @@ public class Main{
 	public static void main(String args[]){
 		int a = 10,b = 20;
 		System.out.println(a > b?"A is greater then B":"B is greater than A");
+		System.out.println("Thank you");
+		
 	}
 }
