6b92a421ce14e232e865d16c34248dc60746353b
Parthasarathy-G <parthasarathy0580@gmail.com>
2025-01-23 22:38:07 +0530

    class man changed

---
+++ git-example/main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class main{
+	public static void main(String args[]){
+		int a = 10,b = 20;
+		System.out.println(a > b?"A is greater then B":"B is greater than A");
+		System.out.println("Thank you");
+	}
+}
