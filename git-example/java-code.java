68f053b9ab7adba9f112a1bad7cf4b3adb759e40
Parthasarathy-G <parthasarathy0580@gmail.com>
2025-01-23 22:42:41 +0530

    Fixed errors

--- git-example/java-code.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Main{
-	public static void main(String args[]){
-		int a = 10,b = 20;
-		System.out.println(a > b?"A is greater then B":"B is greater than A");
-		System.out.println("Thank you");
-	}
-}
